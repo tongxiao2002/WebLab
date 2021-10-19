245c20164f2cfddd46d694b5e9d9121594d8f9b3
Gaoway <gaoway@mail.ustc.edu.cn>
2021-10-19 15:24:40 +0800

    添加tfidf计算，未debug

--- lab1/src/semantic_search.py
+++ lab1/src/semantic_search.py
@@ -1,6 +1,7 @@
 import math
 from re import S
 import nltk
+from nltk.stem.porter import PorterStemmer
 import numpy
 import pickle
 
@@ -10,23 +11,37 @@ N = 306242
 
 class TextInfo():
     def __init__(self, d:float , k:float ):
-        self.dot = d
+        self.dot = d        #
         self.norm = k
 
 
-def get_searchwords(filename:str, indicefile:str):
-    searchtfidf ={}
+def get_searchwords(filename:str, indice:dir):
+    searchtfidf ={}     #searchwords 对应 tfidf
+    Porter_stemmer = nltk.stem.PorterStemmer()
+    norm = 0 
+
     with open(filename, "r") as fin:
         for line in fin:
-            pass
-    return searchtfidf
+            word = Porter_stemmer.stem(line.strip())#标准化 词汇
 
-def compute_vsm(filename:str, indicefile:str):
-    inverse_indice = {}
-    searchtfidf = {}
-    textvsm = []
-    textcosine = []
-    searchnorm = 1      # searchwords norm 
+            if word in indice.keys():
+                idf = math.log(N/len(indice[word])+1)
+                searchtfidf[word] = idf
+                norm += idf*idf
+            else:
+                searchtfidf[word] = 0
+
+    return searchtfidf, math.sqrt(norm)
+
+def compute_vsm(searchwordsfile:str, indicefile:str):
+    picklefile = open(indicefile, 'rb')
+    inverse_indice = pickle.load(picklefile)
+
+    searchtfidf, searchnorm = get_searchwords(searchwordsfile, inverse_indice)
+    textvsm = []        #recode Textinfo 记录cosine 累加的结果·
+    textcosine = []     #由vsm累加的结果得到cos值，最后得到最小的10个数对应的id和uuid
+   # searchwords norm 
+    closestid = []
 
     for word,q in searchtfidf.keys():
         if word in inverse_indice.keys():
@@ -38,8 +53,22 @@ def compute_vsm(filename:str, indicefile:str):
     for info in textvsm:
         textnorm = math.sqrt(info.TextInfo.norm)
         dot = info.TextInfo.dot
-        textcosine.append(dot/(textnorm*searchnorm))
+        cosine = dot/(textnorm*searchnorm)
+        textcosine.append(cosine)    #id是否从0开始
+
+    origincosine = textcosine[:]
+    textcosine.sort()
+
+    for cosine in textcosine[0:9]:
+        closestid.append(origincosine.index(cosine))
+
+    return closestid
 
 
+if __name__ == "__main__":
+    searchwordsfile = "D://WorkPlace/data/searchwords.txt"
+    indicefile =  "D://WorkPlace/data/output/invert_indices.dict"
 
+    closestid = compute_vsm(searchwordsfile, indicefile)
 
+    
